70ff213a69df0045ac1c5979a7f89be5aa711f20
pashupati-git <pashupatic.github@gmail.com>
2026-04-22 15:21:49 +0545

    started work at disclaimer_screen

--- lib/presentation/screens/disclaimer_screen.dart
+++ lib/presentation/screens/disclaimer_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+///Disclaimer screen with legal information.
+/// Demonstrates text-heavy content layout
+/// Uses responsove pading and typography.
+
 class DisclaimerScreen extends ConsumerWidget {
   const DisclaimerScreen({super.key});
 
