1121c7ad7114d51cf9b2b05fe2fba49e8d2033c1
pashupati-git <pashupatic.github@gmail.com>
2026-04-22 16:36:47 +0545

    started work at news categories screen

--- lib/presentation/screens/news_categories_screen.dart
+++ lib/presentation/screens/news_categories_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+///News Categories screen
+///Allows users to select/deselect news categories
+///Uses checkbox list tiles with beautiful icons
+///Changes are persisted automatically through ViewModel
 class NewsCategoriesScreen extends ConsumerWidget{
   const NewsCategoriesScreen({super.key});
 
