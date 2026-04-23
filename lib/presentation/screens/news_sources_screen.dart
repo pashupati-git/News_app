a83ce9927aa0e1a3aa5fa9a0c98405a159d08708
pashupati-git <pashupatic.github@gmail.com>
2026-04-23 15:51:55 +0545

    started work at news sources scrren

--- lib/presentation/screens/news_sources_screen.dart
+++ lib/presentation/screens/news_sources_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class NewsSourcesScreen extends ConsumerWidget{
+class NewsSourcesScreen extends ConsumerWidget {
   const NewsSourcesScreen({super.key});
 
+  ///News SOurces screen
+  ///Allows users to enable/disable and reorder news sources
+  ///Demonstrates ReorderableListView and state management
+  ///Changes are automatically saved to cache.
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text('News Sources')),
       body: const Center(child: Text('News Sources Screen')),
